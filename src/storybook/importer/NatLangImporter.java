17707d4f8c43f3a6f981a9602bd3761433f645c9
Heidi B. Muth <heidibmuth@gmail.com>
2015-03-09 16:25:02 -0400

    Genderize capability added to Importer and NatLangImporter

--- src/storybook/importer/NatLangImporter.java
+++ src/storybook/importer/NatLangImporter.java
@@ -3,8 +3,11 @@ package storybook.importer;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import storybook.ingest.Genderer;
 import storybook.ingest.StanfordCharacterParser;
+import storybook.model.hbn.entity.Gender;
 import storybook.model.hbn.entity.Person;
 import storybook.ui.MainFrame;
 
@@ -21,15 +24,19 @@ public class NatLangImporter extends Importer {
 		StanfordCharacterParser parser = new StanfordCharacterParser(getInputStream());
 		
 		List<String> names = parser.getTokens();
+		Map<String, String> genders = Genderer.genderCharacters(names);
 		
 		for (String n : names) {
 			Person p = new Person();
 			
 			String firstName = n.split(" ")[0];
 			String lastName = n.substring(firstName.length()).trim();
+			String g = genders.get(n);
+			Gender gender = getGender(g);
 			
 			p.setFirstname(firstName);
 			p.setLastname(lastName);
+			p.setGender(gender);
 			
 			persons.add(p);
 		}
